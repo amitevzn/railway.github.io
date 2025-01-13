7cb137c96f995316a951af5e05a9de6c22509648
amit <amit@evoluzn.in>
2025-01-13 13:53:03 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,145 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250111_135921_00_005",
+      "name": "IMG_20250111_135921_00_005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8764923565288143,
+          "pitch": 0.05167033255741771,
+          "rotation": 0,
+          "target": "1-img_20250111_135715_00_004"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4751898335081064,
+          "pitch": 0.24731664329558,
+          "title": "Dustbin",
+          "text": "Dustbins"
+        },
+        {
+          "yaw": -2.5924459914318945,
+          "pitch": 0.2499584471577272,
+          "title": "Drinking Water 1",
+          "text": "Get Fresh Drinking Water"
+        },
+        {
+          "yaw": -0.5743469789735656,
+          "pitch": -0.0956502422423604,
+          "title": "Ladies Toilet",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1358093187805878,
+          "pitch": -0.08191268844451471,
+          "title": "Gents Toilet",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8271485213257215,
+          "pitch": 0.07280157766146722,
+          "title": "Platform 1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20250111_135715_00_004",
+      "name": "IMG_20250111_135715_00_004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.6306133473832967,
+        "pitch": 0.07173977215424188,
+        "fov": 1.2847941013077844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6303981865659143,
+          "pitch": 0.06962822906529098,
+          "rotation": 12.566370614359176,
+          "target": "0-img_20250111_135921_00_005"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2795523519029892,
+          "pitch": -0.05944190971927199,
+          "title": "Gents Toilet",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9202276752833392,
+          "pitch": -0.05064062402818692,
+          "title": "Ladies Toilet",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.940643052004389,
+          "pitch": 0.2440496865378421,
+          "title": "Drinking Water",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
